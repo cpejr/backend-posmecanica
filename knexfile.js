e8092a9f6dad75284a8818a8b6a5c18d3139d15a
Paulo Estêvão <pauloestevao92@gmail.com>
2021-02-16 10:56:53 -0300

    ADD setup inicial

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+// Update with your config settings.
+require("dotenv").config();
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+    },
+
+    migrations: {
+      directory: "./src/database/migrations",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASS,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
---
+++ src/routes.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const connection = require("./database/connection");
+const routes = express.Router();
+module.exports = routes;
