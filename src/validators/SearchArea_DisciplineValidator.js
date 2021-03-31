f0a7f0adc2a1344f5696342edd3eb576a6059ba6
Fernando Cesar <cmnando.silva@gmail.com>
2021-03-31 02:43:22 -0300

    Feat: validator searchArea_discipline feito

---
+++ src/validators/SearchArea_DisciplineValidator.js
@@ -0,0 +1,38 @@
+const { celebrate, Segments, Joi } = require('celebrate');
+
+module.exports = {
+  connect: celebrate({
+    [Segments.HEADERS]: Joi.object()
+      .keys({
+        authorization: Joi.string().required(),
+      })
+      .unknown(),
+    [Segments.PARAMS]: Joi.object().keys({
+      sAd_dis_id: Joi.string()
+        .guid({
+          version: ['uuidv4'],
+        })
+        .required(),
+    }),
+    [Segments.BODY]: Joi.object().keys({
+        sAd_research_ids: Joi.array()
+        .items({
+          sAd_research_id: Joi.string().guid({
+            version: ['uuidv4'],
+          }),
+        })
+        .required(),
+    }),
+  }),
+
+  disconnect: celebrate({
+    [Segments.HEADERS]: Joi.object()
+      .keys({
+        authorization: Joi.string().required(),
+      })
+      .unknown(),
+    [Segments.PARAMS]: Joi.object().keys({
+        search_dis_id: Joi.number().integer().required(),
+    }),
+  }),
+};
