7df75c18f0fdea28b85cafa52aaa3b120cb81eb0
Antonio Britto <antonio.britto.2000@hotmail.com>
2021-04-02 00:36:37 -0300

    Fix: Resolvendo conflito

--- src/models/DisciplineModel.js
+++ src/models/DisciplineModel.js
@@ -43,6 +43,23 @@ module.exports = {
       discipline.professors = relation;
     });
 
+    const studentTable = await connection("student").select("*")
+    const stud_discTable = await connection('student_dis');
+
+    disciplineTable.forEach((discipline) => {
+      const relation = [];
+      const studRelation = stud_discTable.filter(
+        (elements) => elements.sd_dis_id === discipline.discipline_id
+      );
+      studRelation.forEach((ids) => {
+        relation.push(
+          studentTable.find(
+            (student) => student.stud_id === ids.sd_student_id
+          )
+        );
+      });
+      discipline.students = relation;
+    });
     const result = disciplineTable;
     return result;
   },
@@ -52,27 +69,33 @@ module.exports = {
       .where({ discipline_id })
       .select('*')
       .first();
-    const professorTable = await connection('professor').select(
-      'prof_id',
-      'prof_name',
-      'prof_email'
-    );
-    const prof_discTable = await connection('professor_discipline');
+    const studentTable = await connection("student").select('*');
+    const stud_discTable = await connection("student_dis")
+      .where({ sd_dis_id: discipline_id })
+      .select("*")
+    const relations = [];
 
-    const relation = [];
     const profRelation = prof_discTable.filter(
       (elements) => elements.pd_dis_id === disciplineObject.discipline_id
     );
     profRelation.forEach((ids) => {
-      relation.push(
+      relations.push(
         professorTable.find(
           (element) => element.prof_id === ids.pd_professor_id
         )
       );
     });
-    disciplineObject.professors = relation;
+    disciplineObject.professors = relations;
 
+    const relacoes = [];
+    stud_discTable.forEach((ids) => {
+      relacoes.push(
+        studentTable.find(
+          (element) => { element.stud.id === ids.sd_student_id })
+      )
+    })
     const result = disciplineObject;
+    result.students = relacoes;
     return result;
   },
 
