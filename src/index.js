b1c19783b717b2edebc32146a91e5f6d706b3bc7
Antonio Britto <antonio.britto.2000@hotmail.com>
2021-04-18 22:11:35 -0300

    Feat: Colocando URL para acesso à documentação comentada

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(specs, { explorer: true })
+  // http://localhost:3333/api-docs/#/ (URL para acessar documentação)
 );
 
 app.listen(port, () => {
