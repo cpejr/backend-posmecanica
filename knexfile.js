631a751f0f3dfc7d2ca6a8d538c5890a32326f88
Vinicius <viniciusfl1403@gmail.com>
2021-07-18 22:49:41 -0300

    Feat: test enviroment setup

--- knexfile.js
+++ knexfile.js
@@ -25,6 +25,22 @@ module.exports = {
     },
   },
 
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './src/database/migrations',
+    },
+    seeds: {
+      directory: './src/database/seeds',
+    },
+    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+
   production: {
     client: 'pg',
     connection: {
